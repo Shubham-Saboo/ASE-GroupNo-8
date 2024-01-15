f76c9ec6a3bab63116569c46a65ee3fe2a1cad8d
Shubham Saboo <sabooshubham2000@gmail.com>
2024-01-15 16:54:48 -0500

    Rounding off mean

--- src/HW1/main.py
+++ src/HW1/main.py
@@ -7,7 +7,7 @@ def stats(data):
     for col in data.cols.all:
         if isinstance(col, NUM):
             mean = col.mid()
-            print(f"Mean for numerical class '{col.txt}': {mean}")
+            print(f"Mean for numerical class '{col.txt}': {mean:.2f}")
         elif isinstance(col, SYM):
             mode = col.mid()
             print(f"Mode for symbolic class '{col.txt}': {mode}")
