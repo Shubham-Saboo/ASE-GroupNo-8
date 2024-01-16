5ef7794b717dc662d6e209b8489573d24e9f8a46
ananya173147 <ananyamantravadi@gmail.com>
2024-01-16 11:02:52 -0500

    fix key error

--- src/HW2/gate.py
+++ src/HW2/gate.py
@@ -17,18 +17,18 @@ if __name__ == "__main__":
             'num_mid': ts.test_add_and_mid_num,
             'sym_mid': ts.test_add_and_mid_sym
         }
-        if(t['run_tc']=="" or t['run_tc']==None):
+        if(t['run_tc'] == "" or t['run_tc'] is None):
             pass
         elif t['run_tc'] == "all":
             print("Running all test cases!")
             ts.run_tests()
-        elif t['run_test'] != "None":
-            print(f"Running test {t['run_test']}")
+        elif t['run_tc'] != "None":
+            print(f"Running test {t['run_tc']}")
             try:
-                tests[t['run_test']]()
-                print(f"Test {t['run_test']} passed.")
+                tests[t['run_tc']]()
+                print(f"Test {t['run_tc']} passed.")
             except AssertionError as e:
-                print(f"Test {t['run_test']} failed: {e}")
+                print(f"Test {t['run_tc']} failed: {e}")
 
         data = DATA(t['file'])
         print(data.stats())
