e53765cce0149038ae620a4dfa2124b6ea004982
Shubham Saboo <sabooshubham2000@gmail.com>
2024-01-16 00:42:17 -0500

    Updated gate.py

--- src/HW2/gate.py
+++ src/HW2/gate.py
@@ -3,28 +3,25 @@ from data import DATA
 from config import help_str
 from test_suite import TestSuite
 
-
 if __name__ == "__main__":
     t = settings(help_str)
-    if(t['help']):
-        print("You can refer the following help: ")
+
+    if t['help']:
+        print("You can refer to the following help: ")
         print(help_str)
     else:
-        if(t['run_tc']=="all"):
-            print("running all tests!")
-            ts = TestSuite()
+        ts = TestSuite()
+
+        if t['run_tc'] == "all":
+            print("Running all test cases!")
             ts.run_tests()
-        elif(t['run_tc']==""):
-            pass
-        elif(t['run_tc']!="None"):
-            print("running test "+ t['run_tc'])
-            ts = TestSuite()
+        elif t['run_tc'] != "None":
+            print(f"Running test {t['run_tc']}")
             try:
                 tests[t['run_tc']]()
                 print(f"Test {t['run_tc']} passed.")
             except AssertionError as e:
                 print(f"Test {t['run_tc']} failed: {e}")
 
-    data = DATA(t['file'])
-    print(data.stats())
-    
+        data = DATA(t['file'])
+        print(data.stats())
