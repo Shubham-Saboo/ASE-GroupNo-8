eb0bd9d32cd9a1a59558855e09b35b75ac745866
ananya173147 <ananyamantravadi@gmail.com>
2024-01-29 22:54:01 -0500

    format, clean, make code consistent

--- src/HW4/data.py
+++ src/HW4/data.py
@@ -29,7 +29,6 @@ Lua Code:
 '''
 
 class DATA:
-    list_1, list_2, list_3, list_4, list_5, list_6 = [[] for _ in range(6)]
 
     def __init__(self, src=[], fun=None):
         self.rows = []
@@ -38,7 +37,6 @@ class DATA:
             for _, x in csv(src):
                 self.add(x, fun)
         else:
-            # for _, x in enumerate(src):
             self.add(src, fun)
 
     def add(self, t, fun=None):
@@ -65,20 +63,22 @@ class DATA:
             elif isinstance(col, SYM):
                 mode = col.mid()
                 statistics[f"{col.txt}"] = int(mode) if type(mode) == float else mode
-
         return statistics
 
-    
+    list_1, list_2, list_3, list_4, list_5, list_6 = [[] for _ in range(6)]
 
     def gate(self, budget0, budget, some):
         random.seed(set_random_seed())
-        rows = random.sample(self.rows, len(self.rows)) #shuffles the data 
+        rows = random.sample(self.rows, len(self.rows)) 
+        #y values of first 6 examples in ROWS
         DATA.list_1.append(f"1. top6: {[r.cells[len(r.cells)-3:] for r in rows[:6]]}")
+        # y values of first 50 examples in ROWS
         DATA.list_2.append(f"2. top50:{[[r.cells[len(r.cells)-3:] for r in rows[:50]]]}")
         
         rows.sort(key=lambda row: row.d2h(self))
+        # y values of ROW[1]
         DATA.list_3.append(f"3. most: {rows[0].cells[len(rows[0].cells)-3:]}")
-        # print(random.seed)
+
         random.shuffle(rows)
         lite = rows[:budget0]
         dark = rows[budget0:]
@@ -87,41 +87,22 @@ class DATA:
         bests = []
 
         for i in range(budget):
-            best, rest = self.best_rest(lite, int(round(len(lite) ** some,1)))
+            best, rest = self.best_rest(lite, (len(lite) ** some))
             todo, selected = self.split(best, rest, lite, dark)
-            DATA.list_4.append(f"4: rand:{sum(list(map(coerce, random.sample(dark, budget0+i)[0].cells[-3:])))/3}")
-            DATA.list_5.append(f"5: mid: {selected.mid().cells[len(selected.mid().cells)-3:]}")
-            DATA.list_6.append(f"6: top: {best.rows[0].cells[len(best.rows[0].cells)-3:]}")
+            # y values of centroid of (from DARK, select BUDGET0+i rows at random)
+            DATA.list_4.append(f"4. rand:{sum(list(map(coerce, random.sample(dark, budget0+i)[0].cells[-3:])))/3}")
+            # y values of centroid of SELECTED
+            DATA.list_5.append(f"5. mid: {selected.mid().cells[len(selected.mid().cells)-3:]}")
+            # y values of first row in BEST
+            DATA.list_6.append(f"6. top: {best.rows[0].cells[len(best.rows[0].cells)-3:]}")
             stats.append(selected.mid())
-            bests.append(best)
+            bests.append(best.rows[0])
             lite.append(dark.pop(todo))
         return stats, bests
 
-    # def gate(self, randomSeed, budget0, budget, some):
-    #     random.seed(randomSeed)
-    #     rows = random.sample(self.rows, len(self.rows)) #shuffles the data     
-    #     DATA.list_1.append(f"1. top6: {[r.cells[len(r.cells)-3:] for r in rows[:6]]}")
-    #     DATA.list_2.append(f"2. top50:{[[r.cells[len(r.cells)-3:] for r in rows[:50]]]}")
-    #     rows.sort(key=lambda row: row.d2h(self))
-    #     DATA.list_3.append(f"3. most: {rows[0].cells[len(rows[0].cells)-3:]}")
-    #     rows = random.sample(self.rows, len(self.rows))
-    #     lite = rows[:budget0]
-    #     dark = rows[budget0:]       
-    #     stats, bests = [], []
-    #     for i in range(budget):
-    #         best, rest = self.best_rest(lite, len(lite)**some)
-    #         todo, selected = self.split(best, rest, lite, dark)
-    #         DATA.list_4.append(f"4: rand:{sum(list(map(coerce, random.sample(dark, budget0+i)[0].cells[-3:])))/3}")
-    #         DATA.list_5.append(f"5: mid: {selected.mid().cells[len(selected.mid().cells)-3:]}")
-    #         DATA.list_6.append(f"6: top: {best.rows[0].cells[len(best.rows[0].cells)-3:]}")
-    #         stats.append(selected.mid())
-    #         bests.append(best.rows[0])
-    #         lite.append(dark.pop(todo))
-    #     return stats, bests
-
     def split(self, best, rest, lite, dark):
         selected = DATA(self.cols.names, [])
-        max_val = 1E30
+        max_val = 0
         out = 1
 
         for i, row in enumerate(dark, 1):
--- src/HW4/gate.py
+++ src/HW4/gate.py
@@ -30,56 +30,12 @@ if __name__ == "__main__":
             except AssertionError as e:
                 print(f"Test {the['run_tc']} failed: {e}")
 
-        data = DATA(the['file'])
-        # print(data.stats())
         file_path = the['file']
 
-        def learn(data, row, my):
-            my['n'] += 1
-            kl = row.cells[data.cols.klass.at]
-            if my['n'] > 10:
-                my['tries'] += 1
-                my['acc'] += 1 if kl == row.likes(my['datas'])[0] else 0
-            my['datas'][kl] = my['datas'].get(kl, DATA(data.cols.names))
-            my['datas'][kl].add(row)
-
-
-        def bayes():
-            wme = {'acc': 0, 'datas': {}, 'tries': 0, 'n': 0}
-            # n_hypotheses, most, tmp, out = 0, None, None, None  # Add these variables
-            DATA(the['file'], lambda data, the: learn(data, the, wme))
-            accuracy = (wme['acc'] / wme['tries'])*100
-            print(f'The bayes function accuracy for the dataset {the["file"].split("/")[2]} is {accuracy:.2f}%')
-
-        def km():
-            best_accuracy = 0
-            best_k, best_m = None, None
-
-            for k in range(4):
-                for m in range(4):
-                    if m == 0:
-                        print(f'For k = {k} and m = {m}, accuracy is N/A')
-                    else:
-                        the['k'] = k
-                        the['m'] = m
-                        wme = {"acc": 0, "datas": {}, "tries": 0, "n": 0}
-                        DATA(the['file'], lambda data, the: learn(data, the, wme))
-                        accuracy = (wme['acc'] / wme['tries']) * 100
-                        print(f'For k = {k} and m = {m}, accuracy is {accuracy:.2f}%')
-
-
-                    # Update best combination if the current accuracy is higher
-                        if accuracy > best_accuracy:
-                            best_accuracy = accuracy
-                            best_k, best_m = k, m
-
-            print(f'The best combination is: k = {best_k}, m = {best_m}, with accuracy {best_accuracy:.2f}%')
-        
         def gate():
-            budget0, budget, some = 4, 10, 0.5        
-            randomSeeds = random.sample(range(15000),20)
-            for randomSeed in randomSeeds:
-                d = DATA(file_path) #loads the data
+            budget0, budget, some = 4, 10, 0.5
+            for i in range(20):
+                d = DATA(file_path) 
                 d.gate(budget0, budget, some)
 
             print('\n'.join(map(str, DATA.list_1)))
@@ -94,21 +50,5 @@ if __name__ == "__main__":
             print('\n')
             print('\n'.join(map(str, DATA.list_6)))
 
-            # with open('../w4.out', 'w') as file:
-            #     for sublist in DATA.list_1:
-            #         file.write(sublist + '\n')
-            #     for sublist in DATA.list_2:
-            #         file.write(sublist + '\n')
-            #     for sublist in DATA.list_3:
-            #         file.write(sublist + '\n')
-            #     for sublist in DATA.list_4:
-            #         file.write(sublist + '\n')
-            #     for sublist in DATA.list_5:
-            #         file.write(sublist + '\n')
-            #     for sublist in DATA.list_6:
-            #         file.write(sublist + '\n')
-        # bayes()
-        # print("\nThe accuracies calculated using different values of k and m are:")
         gate()
-        # km()
         
