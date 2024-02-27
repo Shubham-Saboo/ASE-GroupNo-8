9668193add929e6ff36d13127098976b6265d627
ananya173147 <ananyamantravadi@gmail.com>
2024-02-27 00:06:26 -0500

    add data functions

--- src/HW6/data.py
+++ src/HW6/data.py
@@ -34,6 +34,10 @@ class DATA:
         u = [round(col.mid(),2) for col in (cols or self.cols.all)]
         return ROW(u)
 
+    def div(self, cols=None):
+        u = [round(col.div(),2) for col in (cols or self.cols.all)]
+        return ROW(u)
+
     def stats(data):
         statistics = {}
         total_rows = max(col.n for col in data.cols.all)
@@ -46,6 +50,88 @@ class DATA:
                 mode = col.mid()
                 statistics[f"{col.txt}"] = int(mode) if type(mode) == float else mode
         return statistics
+
+    list_1, list_2, list_3, list_4, list_5, list_6 = [[] for _ in range(6)]
+
+    def gate(self, budget0, budget, some):
+        random.seed(set_random_seed())
+        rows = random.sample(self.rows, len(self.rows)) 
+        #y values of first 6 examples in ROWS
+        DATA.list_1.append(f"1. top6: {[r.cells[len(r.cells)-3:] for r in rows[:6]]}")
+        
+        # y values of first 50 examples in ROWS
+        DATA.list_2.append(f"2. top50:{[[r.cells[len(r.cells)-3:] for r in rows[:50]]]}")
+        
+        rows.sort(key=lambda row: row.d2h(self))
+        # y values of ROW[1]
+        DATA.list_3.append(f"3. most: {rows[0].cells[len(rows[0].cells)-3:]}")
+
+        random.shuffle(rows)
+        lite = rows[:budget0]
+        dark = rows[budget0:]
+
+        stats = []
+        bests = []
+
+        for i in range(budget):
+            best, rest = self.best_rest(lite, (len(lite) ** some))
+            todo, selected = self.split(best, rest, lite, dark)
+            # y values of centroid of (from DARK, select BUDGET0+i rows at random)
+            selected_rows_rand = random.sample(dark, budget0+i)
+            y_values_sum = [0.0, 0.0, 0.0]
+            for row in selected_rows_rand:
+                y_val = list(map(coerce, row.cells[-3:]))
+                y_values_sum = [sum(x) for x in zip(y_values_sum, y_val)]
+            num_rows = len(selected_rows_rand)
+            y_values_centroid = [round(val / num_rows,2) for val in y_values_sum]
+
+            DATA.list_4.append(f"4: rand:{y_values_centroid}")
+
+            # y values of centroid of SELECTED
+            DATA.list_5.append(f"5. mid: {selected.mid().cells[len(selected.mid().cells)-3:]}")
+
+            # y values of first row in BEST
+            DATA.list_6.append(f"6. top: {best.rows[0].cells[len(best.rows[0].cells)-3:]}")
+
+            stats.append(selected.mid())
+            bests.append(best.rows[0])
+            lite.append(dark.pop(todo))
+        return stats, bests, [bests[-1].cells, round(bests[-1].d2h(self),2)]
+
+
+    def stats_div(self, fun=None, ndivs=None):
+        u = {".N": len(self.rows)}
+        for col in self.cols.all:
+            u[col.txt] = round(col.div() if isinstance(col, SYM) else col.div())
+        return u
+
+    def split(self, best, rest, lite, dark):
+        selected = DATA(self.cols.names, [])
+        max_val = 0
+        out = 1
+
+        for i, row in enumerate(dark, 1):
+            b = row.like(best, len(lite), 2)
+            r = row.like(rest, len(lite), 2)
+            if b > r:
+                selected.add(row)
+
+            tmp = abs(b + r) / abs(b - r + 1E-300)
+            if tmp > max_val:
+                out, max_val = i, tmp
+
+        return out, selected
+
+    def best_rest(self, rows, want):
+        rows.sort(key=lambda a: a.d2h(self))
+        best = DATA(self.cols.names)
+        rest = DATA(self.cols.names)
+        for i, row in enumerate(rows):
+            if i < want:
+                best.add(row)
+            else:
+                rest.add(row)
+        return best, rest
     
     def farapart(self, rows, sortp, a=None, b=None, far=None, evals=0):
         far = int(len(rows) * 0.95) + 1
@@ -61,7 +147,6 @@ class DATA:
             a, b = b, a
         
         return a, b, a.dist(b, self), evals
-
     
     def half(self, rows, sortp, before):
         the_half = min(len(rows) // 2, len(rows))
@@ -89,9 +174,7 @@ class DATA:
             a, b, C, _ = data_new.farapart(data_new.rows, sortp=True)
             current_distance = C
             attempts += 1
-            #print(f"Attempt {attempts}: Current Distance = {C}")
         if current_distance <= target_distance:
-            #print("Far apart points found:")
             print(f"far1: {a.cells}")
             print(f"far2: {b.cells}")
             print(f"distance: {current_distance}")
@@ -148,3 +231,14 @@ class DATA:
         for row in rows or []:
             newData.add(row)
         return newData
+
+    def any50(self, random_seed):
+        random.seed(random_seed)
+        rows = random.sample(self.rows, 50)
+        return [rows[0].cells, round(rows[0].d2h(self),2)]
+
+    def best_100(self, random_seed):
+        random.seed(random_seed)
+        rows = random.sample(self.rows, len(self.rows))
+        rows.sort(key=lambda row: row.d2h(self))
+        return [rows[0].cells, round(rows[0].d2h(self),2)]
