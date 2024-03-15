fc09556995df45503960d1b94f823df7f094137a
Shubham Saboo <sabooshubham2000@gmail.com>
2024-03-14 23:55:17 -0400

    Code for HW7 Task2

--- src/HW6/mylo.py
+++ src/HW6/mylo.py
@@ -6,6 +6,51 @@ from datetime import datetime
 import statistics
 from stats import SAMPLE, eg0
 
+def hw7_part2(the):
+    print("\n")
+    print("\n")
+    d = DATA(src = the['file'])
+    print("date:{}\nfile:{}\nrepeat:{}\nseed:{}\nrows:{}\ncols:{}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),the['file'],"20",the['seed'],len(d.rows), len(d.rows[0].cells)))
+    sortedRows =  sorted(d.rows, key=lambda x: x.d2h(d))
+    print(f"best: {o(sortedRows[0].d2h(d),n=2)}")
+    all = base(d)
+    print(f"tiny: {o(statistics.stdev(all)*0.35,n=2)}")
+    print("#base #bonr9 #rand9 #bonr15 #rand15 #bonr20 #rand20 #rand358 ")
+    eg0([
+        SAMPLE(randN(d,9, the=the), "rand9"),
+        SAMPLE(randN(d,15, the=the), "rand15"),
+        SAMPLE(randN(d,20, the=the), "rand20"), 
+        SAMPLE(randN(d,358, the=the), "rand358"), 
+        SAMPLE(bonrN(d,9, the=the), "bonr9"),
+        SAMPLE(bonrN(d,15,the=the), "bonr15"),
+        SAMPLE(bonrN(d,20, the=the), "bonr20"),
+        SAMPLE(base(d), "base")
+    ])
+
+def base(d):
+    baseline_output = [row.d2h(d) for row in d.rows]
+    return baseline_output
+
+def randN(d, n, the):
+    random.seed(the['seed'])
+    rand_arr = []
+    for _ in range(20):
+        rows = d.rows
+        random.shuffle(rows)
+        rowsN = random.sample(rows,n)
+        rowsN.sort(key=lambda row: row.d2h(d))
+        rand_arr.append(round(rowsN[0].d2h(d),2))
+
+    return rand_arr
+
+def bonrN(d, n, the):
+    bonr_arr = []
+    for _ in range(20):
+        _,_, best_stats = d.gate(the['seed'], 4, n-4, 0.5)
+        bonr_arr.append(best_stats[1])
+
+    return bonr_arr
+
 if __name__ == "__main__":
     the = settings(help_str)
 
@@ -72,5 +117,7 @@ if __name__ == "__main__":
             print("any50\t\t{}\t\t\t\t{}".format(op[0],op[1]))
         print("#")
         print("100%\t\t{}\t\t\t\t{}".format(best[0],best[1]))
+
+        hw7_part2(the)
 
        
