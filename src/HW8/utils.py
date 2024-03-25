0c93d5b9b1da2f5a13fdfdd6a8f74c8d014a6293
Shubham Saboo <sabooshubham2000@gmail.com>
2024-03-25 14:38:04 -0400

    Removed slicing error

--- src/HW8/utils.py
+++ src/HW8/utils.py
@@ -85,3 +85,13 @@ def o(t, n=None, u=None):
                 u.append(f"%s: %s", o(k, n), o(v, n))
 
     return "{" + ", ".join(u) + "}"
+
+def slice(t, go=None, stop=None, inc=None):
+    if go is not None and go < 0:
+        go = len(t) + go
+    if stop is not None and stop < 0:
+        stop = len(t) + stop
+    u = []
+    for j in range((go or 1)//1, (stop or len(t))//1, (inc or 1)//1):
+        u.append(t[j])
+    return u
